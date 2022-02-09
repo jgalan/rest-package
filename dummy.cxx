e676303b9bb7c7bdb3c15360111b5675f0a11b64
Javier Galan <javier.galan@unizar.es>
2022-02-09 18:48:06 +0100

    Adding dummy program

--- dummy.cxx
+++ dummy.cxx
@@ -4,12 +4,16 @@
 // Date : Feb-2022
 //
 
-#include <iostream.h>
+#include <iostream>
 using namespace std;
 
 #include "TRestTools.h"
 
 int main(int argc, char** argv) {
     /// Just a dummy test. It will download this file to $HOME/.rest/download/
-    std::string TRestTools::DownloadRemoteFile("https://github.com/rest-for-physics/framework/README.md");
+    std::string localname = TRestTools::DownloadRemoteFile("https://sultan.unizar.es/gasFiles/gases.rml");
+
+    cout << "File downloaded to : " << localname << endl;
+
+    return 0;
 }
